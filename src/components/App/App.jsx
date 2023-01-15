bbd9488182ab66910797e0d7299f19fd00966230
Viktor Biletskyi <106250265+f0remangod@users.noreply.github.com>
2023-01-15 20:20:37 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { Searchbar } from '../Searchbar/Searchbar';
 import { getImages } from '../../services/api';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
@@ -7,128 +6,114 @@ import { Modal } from '../Modal/Modal';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PacmanLoader from 'react-spinners/PacmanLoader';
+import { useState, useEffect } from 'react';
+import { useCallback } from 'react';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    loadedImages: [],
-    error: false,
-    page: 1,
-    isLoading: false,
-    isModalOpen: false,
-    imageUrl: '',
-    imageInModal: {
-      link: '',
-      alt: '',
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [loadedImages, setLoadedImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [imageInModal, setImageInModal] = useState({
+    link: '',
+    alt: '',
+  });
+
+  const showNotificationAfterQuery = useCallback(
+    images => {
+      if (page === 1) {
+        images.hits.length > 0
+          ? toast.success(`🦄 Wow! We found ${images.total} results!`, {
+              theme: 'dark',
+            })
+          : toast.warn(`Sorry, but there are no results for your query`, {
+              theme: 'dark',
+            });
+      }
     },
-  };
+    [page]
+  );
 
-  async componentDidUpdate(_, prevState) {
-    const { searchQuery, page } = this.state;
-
-    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+  useEffect(() => {
+    if (searchQuery !== '') {
       try {
-        this.setState({ isLoading: true });
+        setIsLoading(true);
 
-        const images = await getImages(searchQuery, page);
-
-        this.setState(state => ({
-          loadedImages: [...state.loadedImages, ...images.hits],
-        }));
-
-        this.showNotificationAfterQuery(images);
+        getImages(searchQuery, page).then(res => {
+          setLoadedImages(prev => [...prev, ...res.hits]);
+          showNotificationAfterQuery(res);
+          setIsLoading(false);
+        });
       } catch (error) {
-        this.setState({ error: true });
-
         toast.error('Oops, something went wrong :(');
-
         console.log(error);
-      } finally {
-        this.setState({ isLoading: false });
       }
     }
-  }
+  }, [page, searchQuery, showNotificationAfterQuery]);
 
-  showNotificationAfterQuery = images => {
-    if (this.state.page === 1) {
-      images.hits.length > 0
-        ? toast.success(`🦄 Wow! We found ${images.total} results!`, {
-            theme: 'dark',
-          })
-        : toast.warn(`Sorry, but there are no results for your query`, {
-            theme: 'dark',
-          });
+  const handleLoadMoreClick = () => {
+    setPage(prev => prev + 1);
+  };
+
+  const handleFormSubmit = ssearchQuery => {
+    if (searchQuery !== ssearchQuery) {
+      setSearchQuery(ssearchQuery);
+      setPage(1);
+      setLoadedImages([]);
     }
   };
 
-  handleLoadMoreClick = () => {
-    this.setState(state => ({ page: state.page + 1 }));
-  };
-
-  handleFormSubmit = searchQuery => {
-    if (this.state.searchQuery !== searchQuery) {
-      this.setState({ searchQuery, page: 1, loadedImages: [] });
-    }
-  };
-
-  handleImageClick = image => {
+  const handleImageClick = image => {
     const { largeImageURL, tags } = image;
 
-    this.setState({
-      isLoading: true,
-      isModalOpen: true,
-      imageInModal: {
-        link: largeImageURL,
-        alt: tags,
-      },
-    });
+    setIsLoading(true);
+    setIsModalOpen(true);
+    setImageInModal({ link: largeImageURL, alt: tags });
   };
 
-  handleModalClose = () => {
-    this.setState({ isModalOpen: false });
+  const handleModalClose = () => {
+    setIsModalOpen(false);
   };
 
-  handleModalImageLoaded = () => {
-    this.setState({ isLoading: false });
+  const handleModalImageLoaded = () => {
+    setIsLoading(false);
   };
 
-  render() {
-    const { loadedImages, isLoading, isModalOpen, imageInModal } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleFormSubmit}></Searchbar>
+  return (
+    <>
+      <Searchbar onSubmit={handleFormSubmit}></Searchbar>
 
-        <ImageGallery
-          images={loadedImages}
-          onClick={this.handleImageClick}
-        ></ImageGallery>
+      <ImageGallery
+        images={loadedImages}
+        onClick={handleImageClick}
+      ></ImageGallery>
 
-        {isLoading && (
-          <PacmanLoader
-            color="#3f51b5"
-            size="30px"
-            cssOverride={{
-              display: 'block',
-              margin: '0 auto ',
-              borderColor: '#3f51b5',
-            }}
-          />
-        )}
+      {isLoading && (
+        <PacmanLoader
+          color="#3f51b5"
+          size="30px"
+          cssOverride={{
+            display: 'block',
+            margin: '0 auto ',
+            borderColor: '#3f51b5',
+          }}
+        />
+      )}
 
-        {loadedImages.length > 0 && isLoading === false && (
-          <Button text="Load more" onClick={this.handleLoadMoreClick}></Button>
-        )}
+      {loadedImages.length > 0 && isLoading === false && (
+        <Button text="Load more" onClick={handleLoadMoreClick}></Button>
+      )}
 
-        {isModalOpen && (
-          <Modal
-            image={imageInModal}
-            onClose={this.handleModalClose}
-            onLoad={this.handleModalImageLoaded}
-          ></Modal>
-        )}
+      {isModalOpen && (
+        <Modal
+          image={imageInModal}
+          onClose={handleModalClose}
+          onLoad={handleModalImageLoaded}
+        ></Modal>
+      )}
 
-        <ToastContainer />
-      </>
-    );
-  }
-}
+      <ToastContainer />
+    </>
+  );
+};
